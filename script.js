61e6ac96ce56b28a596dd57b3229188382614326
Adriano Monteiro <adrianoluizsud@gmail.com>
2022-10-18 09:13:30 -0300

    Mudança no JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     let contagem = document.getElementById("Five");
     let hoje  = new Date();
-    const dataDoCasamento = new Date('19/03/2023 11:11')
+    const dataDoCasamento = new Date('22 december 2022')
     let tempoParaOCasamento = dataDoCasamento - hoje
     let dias = Math.floor(tempoParaOCasamento/(1000 * 60 * 60 * 24))
     let horas = Math.floor(tempoParaOCasamento/(1000 * 60 * 60)) - dias * 24
